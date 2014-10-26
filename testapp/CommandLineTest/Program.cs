760de8b3f28abeccdc1338864a0cf7ec05519d49
Jason Curl <jcurl@arcor.de>
2014-10-26 09:48:42 +0100

    Simple test program to dump results of command line parsing.

---
+++ testapp/CommandLineTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CommandLineTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Argument Length: {0}", args.Length);
+            for (int i = 0; i < args.Length; i++) {
+                Console.WriteLine(" Argument {0}: -=> {1} <=-", i, args[i]);
+            }
+        }
+    }
+}
